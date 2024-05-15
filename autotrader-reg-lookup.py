c88aa5cd047fc330d80f46a9af4b4e4e910651ef
MPDoug <douglas.blackwell@motorpoint.co.uk>
2024-05-15 15:38:30 +0100

    Add files via upload

---
+++ autotrader-reg-lookup.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from urllib.error import URLError
+import pyodbc
+
+@st.cache_data
+def get_autotrader_data(n=1000):
+    cnxn = pyodbc.connect(r'Driver=SQL Server;Server=MP-SQL;Database=AutoTrader;Trusted_Connection=yes;')
+    sql = "select top " + str(n) + " * from [AutoTrader].[dbo].[Vehicles] (nolock) aut"
+    df = pd.read_sql(sql,cnxn)
+    return df.set_index("Registration")
+    
+    
+try:
+    df_at = get_autotrader_data(10000)
+    makes = st.selectbox(
+        "Choose registration", list(df_at.index)
+    )
+    if not makes:
+        st.error("Please select at least one reg.")
+    else:
+        data_at = df_at.loc[makes]
+        st.write("### Autotrader Registration")
+        st.dataframe(data_at.sort_index(), use_container_width=True)
+
+except URLError as e:
+    st.error(
+        """
+        **This demo requires internet access.**
+        Connection error: %s
+    """
+        % e.reason
+    )
